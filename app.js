e1af8c519906f92f3f1050ab57254f59ace510b6
Hugada <hugo.gtz.g@gmail.com>
2018-05-03 10:22:49 -0500

    list

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const siteController     = require("./routes/siteController");
 const locationController = require("./routes/locationController");
 
 // Mongoose configuration
-mongoose.connect("mongodb://localhost/deploy-exercise");
+mongoose.connect("mongodb://hugoih:123456@ds113700.mlab.com:13700/gilgamesh");
 
 // Middlewares configuration
 app.use(logger("dev"));
